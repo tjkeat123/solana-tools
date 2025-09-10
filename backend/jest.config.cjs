9238ef4a158e3925821a2a1369f29f93809a2b69
TJ Keat <tjkeat123@gmail.com>
2025-09-11 01:14:25 +0800

    "test: add unit test for determineTransactionType in src/utils/transactions.ts"
    - make sure the transactions are classified correctly

---
+++ backend/jest.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  setupFilesAfterEnv: [],
+  testTimeout: 30000, // 30 second timeout for network requests
+};
